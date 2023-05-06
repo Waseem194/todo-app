a94400d613f05d800045f235be57dc310c12ed65
Muhammad Waseem <muhammad.waseem@venturedive.com>
2023-05-06 17:12:19 +0500

    Some new changes and new task added

--- src/Components/ToDoLIst/ToDoList.js
+++ src/Components/ToDoLIst/ToDoList.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import ToDo from '../ToDo/ToDo';
- 
-const ToDoList = ({toDoList, handleToggle, handleFilter}) => {
-    return (
-        <div>
-            {toDoList.map(todo => {
-                return (
-                    <ToDo toDo={todo} handleToggle={handleToggle} handleFilter={handleFilter}/>
-                )
-            })}
-            <button style={{margin: '20px'}} onClick={handleFilter}>Clear Completed</button>
-        </div>
-    );
+import React from "react";
+import ToDo from "../ToDo/ToDo";
+
+const ToDoList = ({
+  toDoList,
+  handleToggle,
+  handleCompleted,
+  handleUncompleted,
+}) => {
+  return (
+    <div>
+      {toDoList.map((todo, index) => {
+        return (
+          <ToDo
+            key={index}
+            toDo={todo}
+            handleToggle={handleToggle}
+          />
+        );
+      })}
+      <button style={{ margin: "20px" }} onClick={handleCompleted}>
+        Show Completed
+      </button>
+      <button style={{ margin: "20px" }} onClick={handleUncompleted}>
+        Show Uncompleted
+      </button>
+
+      <button style={{ margin: "20px" }} onClick={handleUncompleted}>
+        Show All
+      </button>
+    </div>
+  );
 };
 
 export default ToDoList;
